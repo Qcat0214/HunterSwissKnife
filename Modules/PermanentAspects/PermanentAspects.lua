3ccefab433d32927f3944a8a56ed0958ecd7ae29
SonitusV <sonitusv@protonmail.com>
2018-02-10 15:54:41 +0300

    Aspects of the Pack, Beast and Wild were added to PermanentAspects

--- Modules/PermanentAspects/PermanentAspects.lua
+++ Modules/PermanentAspects/PermanentAspects.lua
@@ -17,3 +17,21 @@ HunterSwissKnifeModule_PermanentAspects_OnAction["Aspect of the Monkey"] = funct
         CastSpellByName("Aspect of the Monkey");
     end
 end
+
+HunterSwissKnifeModule_PermanentAspects_OnAction["Aspect of the Pack"] = function(slot, checkFlags, checkSelf)
+    if not HunterSwissKnifeCore_IsAuraActive(AURA_PACK, "player", true, false) then
+        CastSpellByName("Aspect of the Pack");
+    end
+end
+
+HunterSwissKnifeModule_PermanentAspects_OnAction["Aspect of the Beast"] = function(slot, checkFlags, checkSelf)
+    if not HunterSwissKnifeCore_IsAuraActive(AURA_BEAST, "player", true, false) then
+        CastSpellByName("Aspect of the Beast");
+    end
+end
+
+HunterSwissKnifeModule_PermanentAspects_OnAction["Aspect of the Wild"] = function(slot, checkFlags, checkSelf)
+    if not HunterSwissKnifeCore_IsAuraActive(AURA_WILD, "player", true, false) then
+        CastSpellByName("Aspect of the Wild");
+    end
+end
